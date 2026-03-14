30e0ee4ae910728d2ed9589d03a19a4c42a86efd
seongbin1013 <plok82890@gmail.com>
2026-03-15 01:27:40 +0900

    과제 1-1, 1-2 최종

--- assignment1_2.c
+++ assignment1_2.c
@@ -1,6 +1,3 @@
-// 실수를 입력받아 고정 소수점 표현으로 변환한 비트패턴을 출력하는 프로그램을 작성하시오.
-// 단, 전체 비트는 16비트, 정수부 8비트, 소수부 8비트로 가정한다. Q8.8 fixed-point, two's complement
-
 #include <stdio.h>
 #include <stdint.h> // int16_t 타입을 사용하기 위해 stdint.h 헤더를 포함
 
