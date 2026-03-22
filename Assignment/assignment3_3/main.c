77ac32eaa1d45d559046cc9030864b41b4e3a30c
seongbin1013 <plok82890@gmail.com>
2026-03-22 23:30:28 +0900

    추가 점수 최최종본

--- Assignment/assignment3_3/main.c
+++ Assignment/assignment3_3/main.c
@@ -8,9 +8,9 @@ int main(int c, char **v) {
     if(c != 3) { 
         fprintf(stderr, "사용법: %s <가로> <세로>\n", v[0]);
         fprintf(stderr, "입력 형식: 분수(A/B), 정수(A), 소수(A.B)\n");
-        fprintf(stderr, "예시: %s 3/7 4/9\n", v[0]);
-        fprintf(stderr, "예시: %s 3 4\n", v[0]);
-        fprintf(stderr, "예시: %s 1.5 2.5\n", v[0]);
+        fprintf(stderr, "예시: 3/7 4/9\n", v[0]);
+        fprintf(stderr, "예시: 3 4\n", v[0]);
+        fprintf(stderr, "예시: 1.5 2.5\n", v[0]);
         return EXIT_FAILURE;
     }
     
@@ -27,7 +27,7 @@ int main(int c, char **v) {
     Fraction two = {2, 1}; // 2를 분수로 표현 (2/1)
     Fraction sum = add(width, height); // 가로와 세로의 합
     Fraction perimeter = multiply(two, sum); // 둘레 = 2 * (가로 + 세로)
-    
+
     Fraction area = multiply(width, height); // 넓이 = 가로 * 세로
 
     char peri_str[100];
