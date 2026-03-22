84ac73971de0193754001feaae7c8b02bdda55b0
seongbin1013 <plok82890@gmail.com>
2026-03-22 23:56:33 +0900

    최최종본

--- Assignment/assignment3_1/main.c
+++ Assignment/assignment3_1/main.c
@@ -22,10 +22,14 @@ int main(void) {
  
     // 연산자에 따라 사칙연산 수행
     switch (op) {
-        case '+': result = add(f1, f2);      break;
-        case '-': result = subtract(f1, f2); break;
-        case '*': result = multiply(f1, f2); break;
-        case '/': result = divide(f1, f2);   break;
+        case '+': result = add(f1, f2);      
+        break;
+        case '-': result = subtract(f1, f2); 
+        break;
+        case '*': result = multiply(f1, f2); 
+        break;
+        case '/': result = divide(f1, f2);   
+        break;
        default:
             fprintf(stderr, "지원하지 않는 연산자입니다.\n");
             return EXIT_FAILURE;
